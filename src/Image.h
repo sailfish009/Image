bfd7c9f6b7d78a506bd7daf3febf29dcac97c129
sailfish009 <sailfish009@gmail.com>
2019-04-09 12:18:03 +0900

    first commit

---
+++ src/Image.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/videoio/videoio.hpp>
+#include <opencv2/highgui/highgui_c.h>
+
+#pragma comment(lib, "opencv_core401d.lib" )
+#pragma comment(lib, "opencv_imgproc401d.lib" )
+#pragma comment(lib, "opencv_highgui401d.lib" )
+// #pragma comment(lib, "opencv_ml401d.lib" )
+// #pragma comment(lib, "opencv_video401d.lib" )
+// #pragma comment(lib, "opencv_features2d401d.lib" )
+// #pragma comment(lib, "opencv_calib3d401d.lib" )
+// #pragma comment(lib, "opencv_imgcodecs401d.lib" )
+// #pragma comment(lib, "opencv_videoio401d.lib" )
+
+using namespace cv;
+
+class Image
+{
+public:
+  Image();
+  ~Image();
+
+public:
+
+};
+
